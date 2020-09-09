f9c1e50b543a08ad3e36877d558cf3c6d5b8513e
wangmingxin03 <wangmingxin03@kuaishou.com>
2020-09-09 10:00:34 +0800

    NettyServer

---
+++ src/main/java/pers/wmx/io/netty/NettyServer.java
@@ -0,0 +1,45 @@
+package pers.wmx.io.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @author: wangmingxin03
+ * @date: 2020-09-08
+ */
+public class NettyServer {
+    public static void main(String[] args) throws InterruptedException {
+        //boss线程组 处理客户端连接
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        //worker线程组 处理客户端连接
+        EventLoopGroup workGroup = new NioEventLoopGroup(1);
+
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        bootstrap.group(bossGroup, workGroup)
+                .channel(NioServerSocketChannel.class)
+                .option(ChannelOption.SO_BACKLOG, 128)
+                .childOption(ChannelOption.SO_KEEPALIVE, true)
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel channel) throws Exception {
+                        //在逻辑链pipeline添加自定义handler
+                        channel.pipeline().addLast(new ServerHandler());
+                    }
+                });
+
+        ChannelFuture channelFuture = bootstrap.bind(8888).sync();
+        System.out.println("Server Starting ...");
+
+        //关闭通道、线程组
+        channelFuture.channel().closeFuture().sync();
+        bossGroup.shutdownGracefully();
+        workGroup.shutdownGracefully();
+    }
+
+}
