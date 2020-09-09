c92c1052856ff69fbfd840a3a44192a22a72ba11
wangmingxin03 <wangmingxin03@kuaishou.com>
2020-09-09 10:24:20 +0800

    NettyClient

---
+++ src/main/java/pers/wmx/io/netty/NettyClient.java
@@ -0,0 +1,33 @@
+package pers.wmx.io.netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * @author: wangmingxin03
+ * @date: 2020-09-09
+ */
+public class NettyClient {
+    public static void main(String[] args) throws InterruptedException {
+        EventLoopGroup group = new NioEventLoopGroup();
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap.group(group)
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        socketChannel.pipeline().addLast(new ClientHandler());
+                    }
+                });
+
+        ChannelFuture channelFuture = bootstrap.connect("localhost", 8888).sync();
+        channelFuture.channel().closeFuture().sync();
+
+    }
+
+}
